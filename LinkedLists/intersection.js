f7f7c0eba04ba0b42ddc792f8a6ddf21f73a91dd
Jacky <jackyleichicago@gmail.com>
2019-04-14 22:15:03 -0700

    Implement check for intersection existing

--- LinkedLists/intersection.js
+++ LinkedLists/intersection.js
@@ -1,3 +1,31 @@
 const intersection = (list1, list2) => {
+  // Get length and tail of both lists
+  // If either length === 0 or the tails aren't the same
+  // There cannot be an intersection
+  const length1 = 0;
+  const length2 = 0;
+  let tail1;
+  let tail2;
+  let curr1 = list1.head;
+  let curr2 = list2.head;
 
+  while (curr1.next) {
+    length1++;
+    curr1 = curr1.next;
+    if (curr1.next === null) {
+      tail1 = curr1;
+    }
+  }
+
+  while (curr2.next) {
+    length2++;
+    curr2 = curr2.next;
+    if (curr2.next === null) {
+      tail2 = curr2;
+    }
+  }
+
+  if (!length1 || !length2 || tail1 !== tail2) {
+    return;
+  }
 }
