6d7bbdf6dde1bc669f92e950f378e6bf13929b85
Jacky <jackyleichicago@gmail.com>
2019-04-26 16:52:27 -0700

    Remove unnecessary exit condition

--- LeetCode/subsets.js
+++ LeetCode/subsets.js
@@ -1,9 +1,6 @@
 var subsets = function(nums) {
   let powerSet = [];
   const getSubsets = (start = 0, array = []) => {
-      if (start === nums.length + 1) {
-          return;
-      }
       powerSet.push(array);
       for (let i = start; i < nums.length; i++) {
           getSubsets(i + 1, array.concat(nums[i]));
