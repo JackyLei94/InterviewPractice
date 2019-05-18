e6bb28b62673a88310fe692fa98ab177c709eea0
Jacky <jackyleichicago@gmail.com>
2019-05-18 04:17:25 -0700

    Implement class solution for arrayShuffle

---
+++ LeetCode/arrayShuffle.js
@@ -0,0 +1,21 @@
+// Class solution
+var Solution = function(nums) {
+  this.nums = nums;
+  this.original = [...nums];
+};
+
+Solution.prototype.reset = function() {
+  for (let i = 0; i < this.nums.length; i++) {
+    this.nums[i] = this.original[i];
+  }
+  return this.nums;
+};
+
+Solution.prototype.shuffle = function() {
+  const nums = this.nums;
+  for (let i = nums.length - 1; i > 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    [nums[i], nums[rand]] = [nums[rand], nums[i]];
+  }
+  return nums;
+};
