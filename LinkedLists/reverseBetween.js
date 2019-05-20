eb8dd73fccbaddee1dd1c72b41c1af3186fbb672
Jacky <jackyleichicago@gmail.com>
2019-05-19 19:00:00 -0700

    Implement solution for reverseBetween

--- LinkedLists/reverseBetween.js
+++ LinkedLists/reverseBetween.js
@@ -1,3 +1,22 @@
 const reverseBetween = (head, m, n) => {
-
+  if (head === null || m > n) {
+    return head;
+  }
+  const dummy = {next: head};
+  let prev = dummy;
+  for (let i = 0; i < m - 1; i++) {
+    prev = prev.next; 
+  }
+  
+  let start = prev.next;
+  let then = start.next;
+  
+  for (let i = 0; i < n - m; i++) {
+    start.next = then.next;
+    then.next = prev.next;
+    prev.next = then;
+    then = start.next;
+  }
+  
+  return dummy.next;
 };
