86cdb846547caebdba9f572a787dd49d820d8d59
Jacky <jackyleichicago@gmail.com>
2019-05-23 23:03:24 -0700

    Implement iterative solution for inorderTraversal

--- LeetCode/inorderTraversal.js
+++ LeetCode/inorderTraversal.js
@@ -1,3 +1,24 @@
+// Iterative solution
+const inorderTraversal = function(root) {
+  const output = [];
+  if (root === null) return output;
+  
+  let stack = [];
+  let curr = root;
+  
+  while (curr || stack.length) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    curr = stack.pop();
+    output.push(curr.val);
+    curr = curr.right;
+  }
+
+  return output;
+};
+
 // Recursive solution
 const inorderTraversal = function(root) {
   const output = [];
