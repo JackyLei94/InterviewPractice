6ff82a1af024d4a58f7828b0e33eba7be6f61116
Jacky <jackyleichicago@gmail.com>
2019-05-23 21:36:41 -0700

    Implement recursive solution to inorderTraversal

--- LeetCode/inorderTraversal.js
+++ LeetCode/inorderTraversal.js
@@ -1,3 +1,21 @@
-const inorderTraversal = root => {
+// Recursive solution
+const inorderTraversal = function(root) {
+  const output = [];
+  if (root === null) return output;
+  dfs(root, output);
+  return output;
+};
+
+const dfs = (node, output) => {
+  if (node.left) {
+    dfs(node.left, output)
+  }
   
+  if (node.val) {
+    output.push(node.val);
+  }
+  
+  if (node.right) {
+    dfs(node.right, output);
+  }
 }
