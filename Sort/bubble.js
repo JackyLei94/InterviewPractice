a6674616a86729ed0380940288d7bcdd2ffefbfd
Jacky <jackyleichicago@gmail.com>
2019-05-25 23:36:39 -0700

    Implement unoptimized solution for bubble sort

--- Sort/bubble.js
+++ Sort/bubble.js
@@ -1,5 +1,13 @@
-const bubbleSort = array => {
-
+const bubbleSort = arr => {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = len - 1; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+      }
+    }
+  }
+  return arr;
 };
 
 // Time complexity:
