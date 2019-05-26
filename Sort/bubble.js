94fa4c9209d55489930a4ac08153edc714a26fad
Jacky <jackyleichicago@gmail.com>
2019-05-25 23:38:22 -0700

    Implement optimized solution for bubble sort

--- Sort/bubble.js
+++ Sort/bubble.js
@@ -1,3 +1,24 @@
+// Optimized
+// Don't check values already bubble sorted (50% increase)
+// Set flag to return if entire array is sorted; (best case n)
+const bubbleSort = arr => {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let sorted = true;
+    for (let j = len - 1; j > i; j--) {
+      if (arr[j] < arr[j - 1]) {
+        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+        sorted = false;
+      }
+    }
+    if (sorted) {
+      return arr;
+    }
+  }
+  return arr;
+};
+
+// Unoptimized
 const bubbleSort = arr => {
   const len = arr.length;
   for (let i = 0; i < len; i++) {
