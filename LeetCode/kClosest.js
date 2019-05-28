3162add1f8648ad086fc66d7a19510d35565c189
Jacky <jackyleichicago@gmail.com>
2019-05-27 22:37:10 -0700

    Implement naive solution for kClosest

--- LeetCode/kClosest.js
+++ LeetCode/kClosest.js
@@ -1,3 +1,9 @@
+// Naive solution
+// Time complexity: nlog(n);
 const kClosest = (points, K) => {
-    
+  return points.sort((a,b) => getDistance(a) - getDistance(b)).slice(0,K);
+};
+
+const getDistance = point => {
+  return Math.sqrt(point[0]**2 + point[1]**2);
 };
