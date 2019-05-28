7af6d22380a7bc06010ada2ba6292996b15e92d3
Jacky <jackyleichicago@gmail.com>
2019-05-27 18:28:34 -0700

    Implement initial solution for quick solution

--- Sort/quick.js
+++ Sort/quick.js
@@ -1,3 +1,18 @@
 const quickSort = arr => {
-
+  if (arr.length === 1 || arr.length === 0) return arr;
+  let less = [];
+  let equal = [];
+  let greater = [];
+  let pivot = arr[arr.length - 1];
+  for (let i = 0; i < arr.length; i++) {
+    const val = arr[i];
+    if (val < pivot) {
+      less.push(arr[i]);
+    } else if (val > pivot) {
+      greater.push(val);
+    } else if (val === pivot) {
+      equal.push(val);
+    }
+  };
+  return quickSort(less).concat(equal).concat(quickSort(greater));
 };
