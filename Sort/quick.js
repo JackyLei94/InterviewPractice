2e07b482f2155cdf610c1269340c1e7bffce453a
Jacky <jackyleichicago@gmail.com>
2019-05-27 21:42:50 -0700

    Implement optimized solution for quick sort

--- Sort/quick.js
+++ Sort/quick.js
@@ -1,3 +1,32 @@
+// Optimized with pointers
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    let pivot = right;
+    let partitionIndex = partition(arr, pivot, left, right)
+    quickSort(arr, left, partitionIndex - 1);
+    quickSort(arr, partitionIndex + 1, right);
+  }
+  return arr;
+}
+
+const partition = (arr, pivot, left, right) => {
+  let pivotVal = arr[pivot];
+  let partitionIndex = left;
+  for (let i = left; i < right; i++) {
+    if (arr[i] < pivotVal) {
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(arr, pivot, partitionIndex);
+  return partitionIndex;
+}
+
+const swap = (arr, i, j) => {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+// Naive with concat
 const quickSort = arr => {
   if (arr.length === 1 || arr.length === 0) return arr;
   let less = [];
