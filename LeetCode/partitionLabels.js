7f07af43d24bdec400c6c5247d7392d2fb8f1ce2
Jacky <jackyleichicago@gmail.com>
2019-06-02 00:53:45 -0700

    Implement MVP solution for partitionLabels

--- LeetCode/partitionLabels.js
+++ LeetCode/partitionLabels.js
@@ -1,3 +1,28 @@
 const partitionLabels = S => {
+  let output = [];
+  let start = 0;
+  let last = getLastIdx(S, start);
+  while (last < S.length) {
+    for (let i = start; i < last; i++) {
+      let idx = getLastIdx(S, i);
+      if (idx > last) {
+        last = idx;
+      }
+    }
+    output.push(last + 1 - start);
+    start = last + 1;
+    last = getLastIdx(S, start);
+  }
+  return output;
+};
 
+const getLastIdx = (arr, start) => {
+  let char = arr[start];
+  let lastIdx;
+  for (let i = start; i < arr.length; i++) {
+    if (arr[i] === char) {
+      lastIdx = i;
+    }
+  }
+  return lastIdx;
 };
