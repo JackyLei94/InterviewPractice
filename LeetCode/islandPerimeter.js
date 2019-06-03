d1d839e3daa9b905dab9b7a2dfd71efe8f0f8e15
Jacky <jackyleichicago@gmail.com>
2019-06-03 14:13:22 -0700

    Implement solution to islandPerimeter

--- LeetCode/islandPerimeter.js
+++ LeetCode/islandPerimeter.js
@@ -1,3 +1,22 @@
 const islandPerimeter = grid => {
-
+  const height = grid.length;
+  const width = height && grid[0].length;
+  let perimeter = 0;
+  
+  const validateLand = (row, col) => {
+    if (row >= height || col >= width || grid[row][col] === 0) return;
+    perimeter -= 2;
+  };
+  
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (grid[i][j]) {
+        perimeter += 4;
+        validateLand(i + 1, j);
+        validateLand(i, j + 1);
+      };
+    }
+  }
+  
+  return perimeter;
 };
