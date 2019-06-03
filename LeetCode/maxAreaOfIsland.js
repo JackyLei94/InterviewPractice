a9fc53da96970e0158fe0bd40f781dc613685118
Jacky <jackyleichicago@gmail.com>
2019-06-03 00:51:27 -0700

    Implement solution for maxAreaOfIsland

--- LeetCode/maxAreaOfIsland.js
+++ LeetCode/maxAreaOfIsland.js
@@ -1,3 +1,27 @@
 const maxAreaOfIsland = grid => {
+  const height = grid.length;
+  const width = height && grid[0].length;
+  let maxArea = 0;
+  let area = 0;
+  
+  const bloom = (row, col) => {
+    if (row < 0 || col < 0 || row === height || col === width || grid[row][col] === 0) return;
+    grid[row][col] = 0;
+    area++;
+    bloom(row + 1, col);
+    bloom(row - 1, col);
+    bloom(row, col + 1);
+    bloom(row, col - 1);
+  }
     
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (grid[i][j]) {
+        bloom(i, j);
+        maxArea = Math.max(area, maxArea);
+        area = 0;
+      }
+    }
+  }
+  return maxArea;
 };
