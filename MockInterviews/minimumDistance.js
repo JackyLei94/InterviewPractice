1164a7c6af3bdf743c0cfee86f7e21729cca8830
Jacky <jackyleichicago@gmail.com>
2019-06-06 17:45:16 -0700

    Implement tests and edge cases

--- MockInterviews/minimumDistance.js
+++ MockInterviews/minimumDistance.js
@@ -1,3 +1,31 @@
 const minimumDistance = (area, numRows, numColumns) => {
 
 };
+
+// Tests
+const grid1 = [
+  [1]
+];
+
+const grid2 = [
+  [1,0,0],
+  [0,0,0],
+  [0,0,9]
+];
+
+const grid3 = [
+  [1,1,1,1,0,0,0,0,0],
+  [0,0,0,1,0,0,0,0,0],
+  [0,1,1,1,1,1,1,1,0],
+  [0,1,0,0,0,0,0,1,0],
+  [0,1,1,1,1,1,1,1,0],
+  [0,9,0,0,0,0,0,0,0],
+];
+
+const grid4 = [];
+
+console.log('Expect: -1. Result: ', minimumDistance(grid1, 1, 1));
+console.log('Expect: -1. Result: ', minimumDistance(grid2, 3, 3));
+console.log('Expect: 10. Result: ', minimumDistance(grid3, 6, 9));
+console.log('Expect: -1. Result: ', minimumDistance(grid4, 0, 0));
+
