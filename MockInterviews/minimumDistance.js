8212599218acbafa52675dd16d83c75882f09218
Jacky <jackyleichicago@gmail.com>
2019-06-06 21:59:03 -0700

    Implement BFS solution

--- MockInterviews/minimumDistance.js
+++ MockInterviews/minimumDistance.js
@@ -1,3 +1,31 @@
+// Breadth First Search
+const minimumDistance = (area, numRows, numColumns) => {
+  if (numRows === 0) return -1;
+  
+  const validate = (row, col, dist) => {
+    if (row < 0 || col < 0 || row >= numRows || col >= numColumns || area[row][col] === 0) return;
+    if (area[row][col] === 9) return queue.push(dist);
+    queue.push([row, col, dist + 1]);
+  };
+
+  let queue = [[0,0,1]];
+  
+  while (queue.length) {
+    let curr = queue.shift();
+    if (typeof(curr) === 'number') return curr;
+    let row = curr[0];
+    let col = curr[1];
+    let dist = curr[2];
+    area[row][col] = 0;
+    validate(row + 1, col, dist);
+    validate(row - 1, col, dist);
+    validate(row, col + 1, dist);
+    validate(row, col - 1, dist);
+  }
+
+  return -1;
+};
+
 // Depth First Search
 const minimumDistance = (area, numRows, numColumns) => {
   let min = Infinity;
@@ -37,8 +65,8 @@ const grid3 = [
 
 const grid4 = [];
 
-console.log('Expect: -1. Result: ', minimumDistance(grid1, 1, 1));
-console.log('Expect: -1. Result: ', minimumDistance(grid2, 3, 3));
+// console.log('Expect: -1. Result: ', minimumDistance(grid1, 1, 1));
+// console.log('Expect: -1. Result: ', minimumDistance(grid2, 3, 3));
 console.log('Expect: 10. Result: ', minimumDistance(grid3, 6, 9));
-console.log('Expect: -1. Result: ', minimumDistance(grid4, 0, 0));
+// console.log('Expect: -1. Result: ', minimumDistance(grid4, 0, 0));
 
